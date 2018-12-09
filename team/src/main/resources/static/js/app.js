74afa151d45a1e1473476488ea1f78e1ae4f5a96
Carlos Marañes <carlosmn1997@gmail.com>
2018-12-09 19:13:54 +0100

    Added retry-after in frontend but in the main page

--- team/src/main/resources/static/js/app.js
+++ team/src/main/resources/static/js/app.js
@@ -41,6 +41,36 @@ $(document).ready(
                     }
                 });
             });
+
+        $("#uploadCsv").submit( function (e) {
+            e.preventDefault();
+            var f = $(this);
+            var formData = new FormData(document.getElementById("uploadCsv"));
+            //formData.append("dato", "valor"); Si queremos añadir mas cosas
+            //formData.append(f.attr("name"), $(this)[0].files[0]);
+            $.ajax({
+                url: "/uploadCSV",
+                type: "post",
+                dataType: "html",
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                statusCode: {
+                    400: function (response) {
+                        alert('Fichero csv malformado');
+                        bootbox.alert('<span style="color:Red;">Fichero csv malformado</span>', function () { });
+                    }
+                }
+            })
+                .done(function (res) {
+                    //$("#mensaje").html("Respuesta: " + res);
+                    $("#myModal").modal();
+                    console.log("FICHERO SUBIDO");
+                    console.log(res);
+                    getCsvStatus(res);
+                });
+        });
     }
     );
 
@@ -49,44 +79,11 @@ function goToCsv() { $("#idContenido").load("csv.html #idContenido"); return tru
 function goToAlive() { $("#idContenido").load("alive.html #idContenido"); return true; }
 
 
-$(function () {
-    $("#uploadCsv").on("submit", function (e) {
-        e.preventDefault();
-        var f = $(this);
-        var formData = new FormData(document.getElementById("uploadCsv"));
-        //formData.append("dato", "valor"); Si queremos añadir mas cosas
-        //formData.append(f.attr("name"), $(this)[0].files[0]);
-        $.ajax({
-            url: "/uploadCSV",
-            type: "post",
-            dataType: "html",
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            statusCode: {
-                400: function (response) {
-                    alert('Fichero csv malformado');
-                    bootbox.alert('<span style="color:Red;">Fichero csv malformado</span>', function () { });
-                }
-            }
-        })
-            .done(function (res) {
-                //$("#mensaje").html("Respuesta: " + res);
-                $("#myModal").modal();
-                console.log("FICHERO SUBIDO");
-                console.log(res);
-                getCsvStatus(res);
-            });
-    });
-});
-
-
 function getCsvStatus(url) {
     $.ajax({
         url: url
     })
-        .done(function (res) {
+        .done(function (res, textStatus, request) {
             //aria-valuenow="50";
             //style="width:50%"
             var percentage = (res.converted / res.total)*100
@@ -99,7 +96,10 @@ function getCsvStatus(url) {
                 var win = window.open(res.uriResult, '_blank');
                 win.focus();
             } else {
-                setTimeout(getCsvStatus(url), 5000);
+                var waitingTime = request.getResponseHeader('Retry-After');
+                setTimeout(function() {
+                    getCsvStatus(url);
+                }, waitingTime*1000)
             }
 
         });
