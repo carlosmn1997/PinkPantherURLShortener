619e83593b6e28f3785f7f91efebe81e64167b4a
Gonzalo Torrecillas <718556@unizar.es>
2018-12-12 20:07:36 +0100

    Sponsor interstitial makeover

--- team/src/main/java/urlshortener/team/service/SponsorServiceImpl.java
+++ team/src/main/java/urlshortener/team/service/SponsorServiceImpl.java
@@ -19,14 +19,34 @@ public class SponsorServiceImpl implements SponsorService {
             "    <script src=\"https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js\"></script>\n" +
             "    <script src=\"js/sponsor.js\" type=\"text/javascript\"></script>\n" +
             "    <script src=\"js/stomp.js\"></script>\n" +
+            "    <link href=\"webjars/bootstrap/3.3.5/css/bootstrap.min.css\" rel=\"stylesheet\"\n" +
+            "          type=\"text/css\"/>\n" +
+            "    <style>\n" +
+            "        #sponsor-buttons a:hover{cursor:pointer;}\n" +
+            "    </style>\n" +
             "</head>\n" +
             "<body>\n" +
-            "<div style=\"width:100%;height:50px;position:fixed;top:0px;\n" +
-            "        background-color:#ff3399;z-index:99999;\">\n" +
-            "    <span id=\"sponsor-skip-button\">Skip</span>\n" +
-            "    <span id=\"sponsor-go-button\" style=\"display:none;\">Go</span>\n" +
-            "    <span id=\"sponsor-countdown\" style=\"display:none;\">Wait 5 seconds...</span>\n" +
+            "<li id=\"sponsor-buttons\" style=\"display:none\">\n" +
+            "    <a id=\"sponsor-skip-button\">Skip ad</a>\n" +
+            "    <a id=\"sponsor-go-button\" style=\"display:none;\">Continue</a>\n" +
+            "    <a id=\"sponsor-countdown\" style=\"display:none;\">Wait 5 seconds...</a>\n" +
             "    <div id=\"token\" style=\"display:none;\">${token}</div>\n" +
+            "</li>\n" +
+            "<div id =\"barraNavegacion\">\n" +
+            "    <script>\n" +
+            "        $(\"#barraNavegacion\").load(\"index.html #barraNavegacion\",function(){\n" +
+            "            $(\"#barraNavegacion .navbar-nav\").empty();\n" +
+            "            var sponsorButtons = $(\"#sponsor-buttons\").clone()[0];\n" +
+            "            $(\"#sponsor-buttons\").remove();\n" +
+            "            $(\"#barraNavegacion .navbar-nav\").append(sponsorButtons);\n" +
+            "            $(\"#sponsor-skip-button\").click(function () {\n" +
+            "                $(\"#sponsor-skip-button\").hide();\n" +
+            "                $(\"#sponsor-countdown\").show();\n" +
+            "                connect();\n" +
+            "            });\n" +
+            "            $(\"#sponsor-buttons\").show();\n" +
+            "        });\n" +
+            "    </script>\n" +
             "</div>\n" +
             "<div id=\"sponsor-body\" style=\"position:absolute;top:50px;\">${sponsorhtml}</div>\n" +
             "</body>\n" +
