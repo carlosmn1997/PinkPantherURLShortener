f0442cdf5669758ddfd16128d0c4c9b46bfa4a70
nico_lera17 <nleralopez@gmail.com>
2019-01-13 10:39:49 +0100

    SOAP still not working

---
+++ team/src/main/java/urlshortener/team/web/SOAP/GetQRRequest.java
@@ -0,0 +1,99 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2019.01.12 a las 09:10:45 PM CET 
+//
+
+
+package urlshortener.team.web.SOAP;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="uri" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="hash" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "uri",
+    "hash"
+})
+@XmlRootElement(name = "getQRRequest")
+public class GetQRRequest {
+
+    @XmlElement(required = true)
+    protected String uri;
+    @XmlElement(required = true)
+    protected String hash;
+
+    /**
+     * Obtiene el valor de la propiedad uri.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * Define el valor de la propiedad uri.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUri(String value) {
+        this.uri = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad hash.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getHash() {
+        return hash;
+    }
+
+    /**
+     * Define el valor de la propiedad hash.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setHash(String value) {
+        this.hash = value;
+    }
+
+}
---
+++ team/src/main/java/urlshortener/team/web/SOAP/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2019.01.12 a las 09:10:45 PM CET 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://qr/web/ws/schema", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package urlshortener.team.web.SOAP;
